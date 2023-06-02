763ab1f12a9db677991d2221b68db7f04290d384
Carlos Rojas <cirojas@uc.cl>
2023-06-02 13:52:01 -0400

    renaming output files

--- scripts/benchmark_wdbench_paths_mdb.sh
+++ scripts/benchmark_wdbench_paths_mdb.sh
@@ -9,29 +9,32 @@ RESULTS_PATH=$RPQ_PAPER_HOME/results
 QUERIES=$RPQ_PAPER_HOME/queries/wdbench_paths.txt
 QUERIES_DUPLICATED=$RPQ_PAPER_HOME/queries/wdbench_paths_cache.txt
 
-for sem in all_shortest_trails all_shortest_simple all
+for SEMANTIC in all_shortest_trails all_shortest_simple all
 do
     # trie naive
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs naive trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-bfs_naive_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_naive_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_trie.csv
 
     # trie cache
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-bfs_cache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_cache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv
 
     # trie full cache
     sync; echo 3 > /proc/sys/vm/drop_caches
-    rm $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES_DUPLICATED $SEMANTIC bfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
-    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv > $RESULTS_PATH/stat_$SEMANTIC-bfs_fullcache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
+    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_fullcache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv
     rm $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
 
     # btree
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs naive btree
+    mv $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_btree.csv $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_naive_btree.csv
 done
 
 for SEMANTIC in simple trails any_simple any_trails any
@@ -39,40 +42,46 @@ do
     # trie naive
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs naive trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-bfs_naive_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_naive_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_trie.csv
 
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC dfs naive trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-dfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-dfs_naive_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-dfs_naive_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-dfs_naive_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-dfs_naive_trie.csv
 
     # trie cache
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-bfs_cache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_cache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv
 
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC dfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-dfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/stat_$SEMANTIC-dfs_cache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-dfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/wdbench_stat_$SEMANTIC-dfs_cache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-dfs_cache_trie.csv
 
     # trie full cache
     sync; echo 3 > /proc/sys/vm/drop_caches
-    rm $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES_DUPLICATED $SEMANTIC bfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
-    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv > $RESULTS_PATH/stat_$SEMANTIC-bfs_fullcache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
+    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv > $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_fullcache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-bfs_cache_trie.csv
     rm $RESULTS_PATH/tmp_stat_$SEMANTIC-bfs_fullcache_trie.csv
 
     sync; echo 3 > /proc/sys/vm/drop_caches
-    rm $RESULTS_PATH/$SEMANTIC-dfs_cache_trie.csv
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES_DUPLICATED $SEMANTIC dfs cache trie
-    python3 scripts/process_trie_stats.py $RESULTS_PATH/$SEMANTIC-dfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-dfs_fullcache_trie.csv
-    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-dfs_fullcache_trie.csv > $RESULTS_PATH/stat_$SEMANTIC-dfs_fullcache_trie.csv
+    python3 scripts/process_trie_stats.py $RESULTS_PATH/wdbench_$SEMANTIC-dfs_cache_trie.csv $TRIE_STATS > $RESULTS_PATH/tmp_stat_$SEMANTIC-dfs_fullcache_trie.csv
+    tail -n 592 $RESULTS_PATH/tmp_stat_$SEMANTIC-dfs_fullcache_trie.csv > $RESULTS_PATH/wdbench_stat_$SEMANTIC-dfs_fullcache_trie.csv
+    rm $RESULTS_PATH/wdbench_$SEMANTIC-dfs_cache_trie.csv
     rm $RESULTS_PATH/tmp_stat_$SEMANTIC-dfs_fullcache_trie.csv
 
     # btree
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC bfs naive btree
+    mv $RESULTS_PATH/wdbench_$SEMANTIC-bfs_naive_btree.csv $RESULTS_PATH/wdbench_stat_$SEMANTIC-bfs_naive_btree.csv
 
     sync; echo 3 > /proc/sys/vm/drop_caches
     python3 scripts/benchmark_wdbench_paths_mdb.py $QUERIES $SEMANTIC dfs naive btree
+    mv $RESULTS_PATH/wdbench_$SEMANTIC-dfs_naive_btree.csv $RESULTS_PATH/wdbench_stat_$SEMANTIC-dfs_naive_btree.csv
 done
